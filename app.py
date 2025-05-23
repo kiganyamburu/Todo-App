8777df0cf8648875192e6fdb6870c1aaf9ba4be2
kiganyamburu <mburukiganya@gmail.com>
2025-05-24 00:00:14 +0300

    made some changes in app.py

--- app.py
+++ app.py
@@ -1,80 +1,92 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, request,redirect
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
-
-# my app 
 app = Flask(__name__)
 Scss(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    """A Model for an Item in the Todo List
 
-class MyTask(db.Model):
+    Args:
+        db (_type_): database model
+
+    Returns:
+        __repr__: string rep.
+    """
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer , default=0)
-    created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    def __repr__(self)-> str:
-        return f"Task {self.id} - {self.content}"
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<Task {self.id}'
 
-
-
-# Route as webpage
-# home page
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=["POST","GET"])
 def index():
-    # Add a new task
-    if request.method == 'POST':
-        current_task = request.form['content']
-        new_task = MyTask(content=current_task)
+    """Main page for App
+
+    Returns:
+        page: home page
+    """
+    if request.method == "POST":
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
         try:
             db.session.add(new_task)
             db.session.commit()
-            return redirect('/')
+            return redirect("/")
         except Exception as e:
             print(f"Error:{e}")
-            return f"Error: {e}"
-        
-    # see all current tasks
+            return f'Error:{e}'
     else:
-        task = MyTask.query.order_by(MyTask.created).all()
-        return render_template('index.html', tasks=task)
-    
-    # Delete a task
-@app.route('/delete/<int:id>')
-def delete(id:int):
-    delete_task = MyTask.query.get_or_404(id)
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html',tasks=tasks)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    """delete an item from the todo list
+
+    Args:
+        id (int): uuid for each item in the todo list
+
+    Returns:
+        redirect: delete and return to home
+    """
+    task_to_delete = Todo.query.get_or_404(id)
     try:
-        db.session.delete(delete_task)
-        db.session.commit()
-        return redirect('/')
+        db.session.delete(task_to_delete)
+        db.session.commit()  
+        return redirect("/")
     except Exception as e:
-        return f"Error: {e}"
-    
-    
-@app.route("/edit/<int:id>", methods=['GET', 'POST'])
-def edit(id:int):
-    task = MyTask.query.get_or_404(id)
-    if request.method == 'POST':
+        return f"Error:{e}"
+
+@app.route("/update/<int:id>", methods=["GET","POST"])
+def update(id):
+    """update an item from the todo list
+
+    Args:
+        id (int): uuid for each item in the todo list
+
+    Returns:
+        redirect: update and return to home
+    """
+    task = Todo.query.get_or_404(id)
+    if request.method == "POST":
         task.content = request.form['content']
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect("/")
         except Exception as e:
-            return f"Error: {e}"
+            print(f"ERROR:{e}")
+            return "Error"
     else:
-        return render_template('edit.html', task=task)
-        
+        return render_template("update.html", task=task)
 
-    
-
-
-
-if __name__ =="__main__":
+if __name__ in "__main__":
     with app.app_context():
         db.create_all()
-
     app.run(debug=True)
