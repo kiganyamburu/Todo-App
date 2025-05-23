19dd55926ff815c953431278fa2dfffcdf4594ea
kiganyamburu <mburukiganya@gmail.com>
2025-05-23 23:47:01 +0300

    feat(app.py):creating edit functionality

--- app.py
+++ app.py
@@ -54,6 +54,22 @@ def delete(id:int):
     except Exception as e:
         return f"Error: {e}"
     
+    
+@app.route("/edit/<int:id>", methods=['GET', 'POST'])
+def edit(id:int):
+    task = MyTask.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            return f"Error: {e}"
+    else:
+        return "home"
+        
+
+    
 
 
 
